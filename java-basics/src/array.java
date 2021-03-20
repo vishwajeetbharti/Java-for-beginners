571fee5a9341ff29d0e685f63d800f31976f44bf
Vishwaajeet Bharti <71969138+vishwajeetbharti@users.noreply.github.com>
2021-03-20 22:35:38 +0530

    Array

--- java-basics/src/array.java
+++ java-basics/src/array.java
@@ -1,11 +1,20 @@
-
+import java.util.*;
 public class array {
 
 	public static void main(String[] args) {
-		int marks[]  = {32,23,34,34,43};
+		Scanner sc= new Scanner(System.in);
+
+		int marks[]  = {32,23,34,34,43};//Declare + Initialize
 		for(int i=0;i<marks.length;i++)
 		System.out.println(marks[i]);
-		
+		int[] score;// Declaration.
+		score = new int[5];// Allocation.
+		System.out.println("Enter the score");
+		for(int i=0;i<score.length;i++)
+			score[i]=sc.nextInt();
+		for(int i=0;i<marks.length;i++)
+			System.out.println(score[i]);		
 	}
+	
 
 }
