a7c2a338db7730f8266023e84c852ae40867c2da
Vishwaajeet Bharti <71969138+vishwajeetbharti@users.noreply.github.com>
2021-04-27 22:12:01 +0530

    strings keywords

---
+++ java-basics/src/string_method.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class string_method {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String name,lower,upper,tr,sh,sh2,re;
+		System.out.println("Enter your name:-");
+		name = sc.nextLine();
+		int value = name.length();
+		System.out.println("1:-" + value);
+		lower =name.toLowerCase();
+		System.out.println("2:-"+lower);
+		upper =name.toUpperCase();
+		System.out.println("3:-"+upper);
+		tr =name.trim();
+		System.out.println("5:-"+tr);
+		sh =name.substring(2);
+		System.out.println("6:-"+sh);
+		sh2 =name.substring(0,2);
+		System.out.println("7:-"+sh2);
+		re =name.replace('s','t');
+		System.out.println("8:-"+re);
+		System.out.println("9:-"+name.startsWith("se"));
+		System.out.println("10:-"+name.endsWith("ar"));
+		System.out.println("11:-"+name.charAt(2));
+		System.out.println("13:-"+name.indexOf(0,'s'));
+		
+		
+	}
+
+}
