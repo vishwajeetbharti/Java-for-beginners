1ab1954fc09ff5fef98eb0d242f872c968ed6a64
Vishwaajeet Bharti <71969138+vishwajeetbharti@users.noreply.github.com>
2021-04-27 19:54:34 +0530

    Strings in java

---
+++ java-basics/src/strings.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class strings {
+
+	public static void main(String[] args) {
+		//String name = new String("setu");
+        //String name = "setu";
+        System.out.print("The name is: ");
+       // System.out.print(name);
+        //int a = 6;
+       // float b = 5.6454f;
+        //System.out.printf("The value of a is %d and value of b is %8.2f", a, b);
+        //System.out.format("The value of a is %d and value of b is %f", a, b);
+        Scanner sc = new Scanner(System.in);
+       // String st = sc.next();
+       String st = sc.nextLine();
+         System.out.println(st);
+//		  sc.close();
+
+	}
+
+}
