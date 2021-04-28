9125b2a4a73bbfb53c0dd58ea1ec9397857e3d0d
Vishwaajeet Bharti <71969138+vishwajeetbharti@users.noreply.github.com>
2021-04-28 10:31:42 +0530

    Update string_method.java

--- java-basics/src/string_method.java
+++ java-basics/src/string_method.java
@@ -18,9 +18,9 @@ public class string_method {
 		System.out.println("6:-"+sh);
 		sh2 =name.substring(0,2);
 		System.out.println("7:-"+sh2);
-		re =name.replace('s','t');
+		re =name.replace('y','v');
 		System.out.println("8:-"+re);
-		System.out.println("9:-"+name.startsWith("se"));
+		System.out.println("9:-"+name.startsWith("yu"));
 		System.out.println("10:-"+name.endsWith("ar"));
 		System.out.println("11:-"+name.charAt(2));
 		System.out.println("13:-"+name.indexOf(0,'s'));
