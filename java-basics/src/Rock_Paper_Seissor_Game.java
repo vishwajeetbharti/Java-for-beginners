235c7803292305898c4aca775e279ce7ab4f3920
Vishwaajeet Bharti <71969138+vishwajeetbharti@users.noreply.github.com>
2021-04-29 19:15:27 +0530

    Update Rock_Paper_Seissor_Game.java

--- java-basics/src/Rock_Paper_Seissor_Game.java
+++ java-basics/src/Rock_Paper_Seissor_Game.java
@@ -1,6 +1,6 @@
 import java.util.Random;
 import java.util.*;
-public class Random_character {
+public class Rock_Paper_Seissor_Game {
 
 	public static void main(String[] args) {
 		Scanner sc= new Scanner(System.in);
