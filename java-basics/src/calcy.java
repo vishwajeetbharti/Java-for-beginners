c3daf824c70dbafca863743267c5aa952adcd734
Vishwajeet Bharti <vishwajeetbharti@hotmail.com>
2021-08-17 22:15:22 +0530

    Calculater
    Calculater for #User.

---
+++ java-basics/src/calcy.java
@@ -0,0 +1,140 @@
+import java.util.Scanner;
+class  opreater{
+	private float value1,value2;
+	public float output;
+	
+	public void setinput(float userinput) {
+		value1 =userinput;
+	}
+	
+	public void setinput2(float userinput) {
+		value2 =userinput;
+	}
+	public void output1() {
+		System.out.println(output);
+		//return output;
+	}
+	
+	public void output(char operator) {
+		switch(operator){
+		case '+':
+		    output= value1 +value2;
+		    value1 =0;
+		    value1 =output;
+		break;
+		case '-':
+			output = value1 -value2;
+			value1 =0;
+		    value1 =output;
+		break;
+		case '*':	
+		    output= value1 *value2;
+		    value1 =0;
+		    value1 =output;
+		break;
+		case '/':
+			output= value1 /value2;
+			value1 =0;
+		    value1 =output;
+		break;
+	    }
+	}
+}
+public class calcy {
+
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		opreater User = new opreater();
+		float userInput;
+		char operat;
+		boolean A = true,k=true;
+		System.out.println("Calculater Started:-");
+		userInput = sc.nextFloat();
+		
+		do {
+			//System.out.println("jjj");
+			operat =sc.next().charAt(0);
+		switch(operat){
+		case '+':
+			if(A) {
+				User.setinput(userInput);
+				userInput=0;
+				//System.out.println("Enter the 2nd number:-");
+				userInput = sc.nextFloat();
+				User.setinput2(userInput);
+				userInput=0;
+				User.output('+');
+				A= false;
+			}
+			else {
+				userInput = sc.nextFloat();
+				User.setinput2(userInput);
+				userInput=0;
+				User.output('+');
+			}
+		break;
+		case '-':
+			if(A) {
+				User.setinput(userInput);
+				userInput=0;
+				userInput = sc.nextFloat();
+				User.setinput2(userInput);
+				User.output('-');
+				A= false;
+			}
+			else {
+				userInput = sc.nextFloat();
+				User.setinput2(userInput);
+				userInput=0;
+				User.output('-');
+			}
+			
+		break;
+		case '*':
+			if(A) {
+				User.setinput(userInput);
+			userInput=0;
+			userInput = sc.nextFloat();
+			User.setinput2(userInput);
+			User.output('*');
+			A= false;
+		}	
+			else {
+				userInput = sc.nextFloat();
+				User.setinput2(userInput);
+				userInput=0;
+				User.output('*');
+			}
+			
+		break;
+		case '/':
+			if(A) {
+				User.setinput(userInput);
+				userInput=0;
+				userInput = sc.nextFloat();
+				User.setinput2(userInput);
+				User.output('/');
+				A= false;
+			}
+			else {
+				userInput = sc.nextFloat();
+				User.setinput2(userInput);
+				userInput=0;
+				User.output('/');
+			}
+			
+		break;
+		case 'e':
+			User.output1();
+			k = false;
+			break;
+		default:
+		    System.out.println("Error operator.");
+		sc.close(); 
+		}
+	} while(k);
+         
+	}
+
+}
+
