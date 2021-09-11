6e8b20ae3be1dcedb619d8c684dac7ed0a029273
Vishwajeet Bharti <vishwajeetbharti@hotmail.com>
2021-09-11 13:41:56 +0530

    Create arraylist.java

---
+++ java-basics/src/arraylist.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class arraylist {
+	public static void main(String[] args) {
+		 String string = "yukati weds vishwajeet bharti";
+		  
+	        List<Character> chars = new ArrayList<>();
+	  
+	        for (char ch : string.toCharArray()) {
+	  
+	            chars.add(ch);
+	        }
+	  
+	        System.out.println(chars);
+	}
+}
