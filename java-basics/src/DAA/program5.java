dfea237d3a4ad27c994ad3e3c2a95df875e7826a
Vishwaajeet Bharti <71969138+vishwajeetbharti@users.noreply.github.com>
2022-08-31 18:18:04 +0530

    changes

--- java-basics/src/DAA/program5.java
+++ java-basics/src/DAA/program5.java
@@ -38,24 +38,22 @@ public class program5 {
         int[] a=new int[5000];
         Scanner take =new Scanner(System.in);
         Random rand= new Random();
-        System.out.println("Enter n: ");
+        System.out.println("Enter n : ");
         n= take.nextInt();
         for (i=0;i<n;i++){
             a[i]=rand.nextInt(1000);
-            System.out.println(" "+a[i]);
+            System.out.print(" "+a[i]);
         }
         System.out.println();
-        long t1 =System.nanoTime();
+        long t1 =System.currentTimeMillis();
         divide(a,0,n-1);
-        long t2 =System.nanoTime();
+        long t2 =System.currentTimeMillis();
         System.out.println("***Sorted list***");
         for(i=0;i<n;i++){
-            System.out.println(" "+a[i]);
+            System.out.print(" "+a[i]);
         }
         System.out.println();
         double t3=(t2-t1);
-        System.out.println("Time taken for the merge sort in nano sec : "+t3);
-        double sec=(double)t3/10000000000.0;
-        System.out.println("Which is "+sec+" Seconds.");
+        System.out.println("Time taken : "+t3);
     }
 }
