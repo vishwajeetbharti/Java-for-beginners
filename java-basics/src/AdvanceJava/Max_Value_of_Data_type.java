57163803139d94cb57d728034f942d01d26584e9
Vishwaajeet Bharti <71969138+vishwajeetbharti@users.noreply.github.com>
2022-10-09 15:04:46 +0530

    Data Types Max value and compare with CurrentTimeMillis

---
+++ java-basics/src/AdvanceJava/Max_Value_of_Data_type.java
@@ -0,0 +1,11 @@
+package AdvanceJava;
+public class Max_Value_of_Data_type {
+    public static void main(String[] args) {
+        System.out.println("The max value of Long:-   "+Long.MAX_VALUE);
+        System.out.println("The max value of Integer:-    "+Integer.MAX_VALUE);
+        System.out.println("\nThe value of current time in ms:-    " + System.currentTimeMillis());
+        System.out.println("\nThe max value of Double:-   "+Double.MAX_VALUE);
+        System.out.println("The max value of Char:-    "+Character.MAX_VALUE);
+        System.out.println("The max value of Float:-   "+Float.MAX_VALUE);
+    }
+}
